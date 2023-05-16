f335f4317265ee3175488c04f24c89d0290f2bef
inamnk28 <inamnkabi1@gmail>
2023-05-16 10:10:30 +0200

    firstcommit

---
+++ index.js
@@ -0,0 +1,14 @@
+// This Is My First Time Using JS //
+console.log('Hello World');
+const myName ="Inam"
+const age= 19
+console.log(myName);
+console.log(age);
+
+const firstName ="Timothy"
+const surname = "Barry"
+const aGe = 12
+const occupation = "Idiot"
+const gender = "Ask Jesus"
+console.log(firstName, surname, aGe, occupation, gender);
+
